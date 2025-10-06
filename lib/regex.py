40eb1ed323d9a0e20b1a464f319b74e412082095
Matara Kemari <drkemari@gmail.com>
2025-10-06 15:31:38 +0300

    Final

--- lib/regex.py
+++ lib/regex.py
@@ -1,13 +1,39 @@
 import re
 
-# NOTE: There are only a few tests included, so multiple solutions will work.
-# Feel free to encourage students to find oversights and add tests to this lab!
+# Matches proper names like:
+# - John Cena
+# - Anya Taylor-Joy
+# - D'Angelo
+# Does NOT match:
+# - john cena
+# - John   Cena
+# - J0hn C3na
+name_regex = re.compile(
+    r"^[A-Z][a-z]*(?:['-]?[A-Z]?[a-z]+)*(?: [A-Z][a-z]*(?:['-]?[A-Z]?[a-z]+)*)?$"
+)
 
-name = r""
-name_regex = re.compile(name)
 
-phone_number = r""
-phone_regex = re.compile(phone_number)
+# Matches valid phone formats:
+# - 5555555555
+# - 555-555-5555
+# - (555) 555-5555
+# Does NOT match:
+# - 555555555 (too short)
+# - 555--555--5555
+# - aaaaaaaaaa
+phone_regex = re.compile(
+    r"^(?:\d{10}|\d{3}-\d{3}-\d{4}|\(\d{3}\) \d{3}-\d{4})$"
+)
 
-email_address = r""
-email_regex = re.compile(email_address)
+# Matches valid email formats:
+# - johncena@wwe.com
+# - john.cena@wwe.com
+# - john.cena123@wwe.com
+# Does NOT match:
+# - 123john.cena@wwe.com
+# - johncena.com
+# - john$cena@wwe.com
+# - johncena@
+email_regex = re.compile(
+    r"^[A-Za-z][A-Za-z0-9\.]*@[A-Za-z]+\.[A-Za-z]{2,}$"
+)
